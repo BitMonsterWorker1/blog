3e9066310210e6cda42b5e977748bee92643eb26
MAX <30717231+BitMonsterWorker1@users.noreply.github.com>
2017-11-20 16:39:54 -0500

    controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
 
   def show
   end
-end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        session[:user_id] = @user.id
+        redirect_to '/dashboard', notice: 'U have signed up!'
+      else
+        redirect_to new_user_path, notice: "error:
+        \n#{@user.errors.full_messages.join('/n')}"
+    end
+  end
+ private
+
+ def user_params
+   params.require(:user).permit(:username, :password)
+
+ end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, :password, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {in: 5..8 }
+  validates :username, length: {in: 5..8 }
 end
